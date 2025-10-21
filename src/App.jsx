a3c26e7a43726a5a25279ba97758eae13ac08152
baruch9353 <baruchminzberg@gmail.com>
2025-10-21 15:49:44 +0300

    add dashboard route and new components: AddButton, Organization, SearchBar, Terrorist, Layout, and pages for dashboard, organizations, and terrorists

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<div>Layout</div>} />
+      <Route path="/dashboard" element={<div>Dashboard</div>} />
       <Route path="/organizations" element={<div>Organizations</div>} />
       <Route path="/terrorists" element={<div>Terrorists</div>} />
-      <Route path="/singleOrganization" element={<div>SingleOrganization</div>} />
-      <Route path="/addOrganization" element={<div>AddOrganization</div>} />
-      <Route path="/singleTerrorist" element={<div>SingleTerrorist</div>} />
-      <Route path="/addTerrorist" element={<div>AddTerrorist</div>} />
     </Routes>
   )
 }
