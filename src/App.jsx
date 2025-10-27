634349d73d975b6a31cc2b187b62794617969da4
baruch9353 <baruchminzberg@gmail.com>
2025-10-27 11:32:08 +0200

    Added a new Layout component with a responsive AppBar and Drawer for navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router"
+import Layout from "./components/layout/Layout.jsx"
+import Dashboard from "./pages/Dashboard.jsx"
+import OrganizationsPage from "./pages/OrganizationsPage.jsx"
+import TerroristsPage from "./pages/TerroristsPage.jsx"
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Layout</div>} />
-      <Route path="/dashboard" element={<div>Dashboard</div>} />
-      <Route path="/organizations" element={<div>Organizations</div>} />
-      <Route path="/terrorists" element={<div>Terrorists</div>} />
+      <Route path="/" element={<Layout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="organizations" element={<OrganizationsPage />} />
+        <Route path="terrorists" element={<TerroristsPage />} />
+      </Route>
     </Routes>
   )
 }
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,96 @@
+import SentinelEye from "../../assets/SentinelEye.png";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemText,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { styled } from "@mui/material/styles";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+const StyledAppBar = styled(AppBar)({
+  background: "#a7d8f0",
+  color: "#036a12",
+});
+
+const StyledToolbar = styled(Toolbar)({
+  justifyContent: "space-between",
+});
+
+const Title = styled(Typography)({
+  fontSize: "3rem",
+  cursor: "pointer",
+});
+
+const Logo = styled("img")({
+  height: 80,
+  padding: 10,
+  borderRadius: 4,
+});
+
+const DrawerBox = styled(Box)({
+  width: 220,
+});
+
+const DrawerBoxStyled = styled(DrawerBox)({
+  textAlign: "center",
+  color: "#036a12",
+});
+
 export default function Layout() {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const items = [
+    { text: "Dashboard", path: "/dashboard" },
+    { text: "Organizations", path: "/organizations" },
+    { text: "Terrorists", path: "/terrorists" },
+  ];
+
   return (
-    <div>Layout</div>
-  )
+    <>
+      <StyledAppBar position="static">
+        <StyledToolbar>
+          <IconButton color="inherit" onClick={() => setOpen(true)}>
+            <MenuIcon fontSize="large" />
+          </IconButton>
+          <Title onClick={() => navigate("/dashboard")}>Sentinel Eye</Title>
+          <Logo
+            src={SentinelEye}
+            alt="Sentinel Eye"
+            onClick={() => navigate("/dashboard")}
+          />
+        </StyledToolbar>
+      </StyledAppBar>
+
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        <DrawerBoxStyled
+          onClick={() => setOpen(false)}
+          onKeyDown={() => setOpen(false)}
+        >
+          <List subheader={<Logo src={SentinelEye} alt="Sentinel Eye" />}>
+            <hr />
+            {items.map((item) => (
+              <ListItemButton
+                key={item.path}
+                onClick={() => {
+                  setOpen(false);
+                  navigate(item.path);
+                }}
+              >
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            ))}
+          </List>
+        </DrawerBoxStyled>
+      </Drawer>
+    </>
+  );
 }
